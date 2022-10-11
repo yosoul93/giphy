290f2932829f33fc02cf6cb30b4178a205279dbe
Kevin Ryda <96450879+SpartanDAO@users.noreply.github.com>
2022-10-11 17:01:57 +0900

    created a search list template with style for listing the search result

--- src/app/features/search/list/list.component.ts
+++ src/app/features/search/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchData } from '../search.model';
 
 @Component({
   selector: 'app-search-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchListComponent implements OnInit {
 
+  @Input() public searchList: SearchData[];
   constructor() { }
 
   ngOnInit(): void {
