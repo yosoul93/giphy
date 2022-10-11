d6416fdf362f9124333c0a5b0de717226ad78d4d
Kevin Ryda <96450879+SpartanDAO@users.noreply.github.com>
2022-10-11 16:54:42 +0900

    import httpclient module to allow http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +15,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
     CoreModule
   ],
   providers: [],
