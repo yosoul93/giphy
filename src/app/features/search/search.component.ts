1450a1c963b739bba32db39fb7ec1c6f5fe9c7c9
Kevin Ryda <96450879+SpartanDAO@users.noreply.github.com>
2022-10-11 16:53:10 +0900

    subscribe to search data request from the template and also pagination and import angular material modules needed to render the view

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { SearchService } from './search.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime, takeUntil, switchMap, map } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { SearchPagination, SearchData } from './search.model';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy, AfterViewInit {
+  
+  @ViewChild(MatPaginator) private readonly _paginator: MatPaginator;
 
-  constructor() { }
+  public isLoading: boolean = false;
+  public pageLimit: number; 
+  public pagination: SearchPagination;
+  public searchData$: Observable<SearchData[]>;
+  public readonly searchInputControl: FormControl = new FormControl();
+
+  private searchedQuery: string;
+  private readonly _unsubscribeAll: Subject<any> = new Subject();
+
+  constructor(
+    private readonly _searchService: SearchService,
+  ) { }
 
   ngOnInit(): void {
+    this.pageLimit = this._searchService.PAGE_LIMIT;
+    
+    this._searchService.pagination$
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((pagination: SearchPagination) => {
+        this.pagination = pagination;
+      });
+
+    this.searchData$ = this._searchService.searchData$;
+
+    this.searchInputControl.valueChanges
+      .pipe(
+        takeUntil(this._unsubscribeAll),
+        debounceTime(300),
+        switchMap((query) => {
+          this.searchedQuery = query;
+          this.isLoading = true;
+          return this._searchService.getSearchData({
+            q: query,
+            offset: 0
+          });
+        }),
+        map(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe();
+  }
+  
+  ngAfterViewInit(): void {
+    this._paginator.page.pipe(
+      switchMap(() => {
+        this.isLoading = true;
+        const offset = this._paginator.pageIndex*this.pageLimit;
+        return this._searchService.getSearchData({
+          q: this.searchedQuery,
+          offset: offset
+        });
+      }),
+      map(() => {
+       this.isLoading = false;
+      })
+    )
+    .subscribe();
   }
 
+  ngOnDestroy(): void{
+    this._unsubscribeAll.next(null);
+    this._unsubscribeAll.complete();
+  }
 }
--- src/app/features/search/search.module.ts
+++ src/app/features/search/search.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { SearchDetailComponent } from './detail/detail.component';
 import { SearchListComponent } from './list/list.component';
 import { SearchComponent } from './search.component';
 
 import { searchRoutes } from './search.routing';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SearchService } from './search.service';
 
 @NgModule({
   declarations: [
@@ -15,8 +24,17 @@ import { searchRoutes } from './search.routing';
   ],
   imports: [    
     RouterModule.forChild(searchRoutes),
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
   ],
-  providers: []
+  providers: [SearchService]
 })
 export class SearchModule {
 }
